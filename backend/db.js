481eceb8d592c110b6ce676a2a3e6cca0ef7b0d8
Priyank-2005 <priyank.bohra.31@gmail.com>
2025-12-09 19:31:48 +0530

    fix: db and env

--- backend/db.js
+++ backend/db.js
@@ -10,8 +10,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: parseInt(process.env.DB_PORT) || 3306,
-    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
